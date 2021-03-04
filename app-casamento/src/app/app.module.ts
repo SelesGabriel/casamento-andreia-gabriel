c79bbfd248b08ad0ac5b52313aa3289e4227b533
gadasilvas <gabriel.seles@engdb.com.br>
2021-03-04 01:12:37 -0300

    changes

--- app-casamento/src/app/app.module.ts
+++ app-casamento/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { ConvidadosComponent } from './convidados/convidados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent,
+    ConvidadosComponent
   ],
   imports: [
     BrowserModule,
