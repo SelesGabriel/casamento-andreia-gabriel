a5484756ee15f3e24aa82d91a4b7b701db99ebf6
gadasilvas <gabriel.seles@engdb.com.br>
2021-03-05 16:01:30 -0300

    completando design site

--- app-casamento/src/app/header/header.component.ts
+++ app-casamento/src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { interval, Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  dataCasamento = new Date("Sep 25 2021");
+  dataAtual = new Date();
+  timeDifference: any;
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+  diasRestantes: any;
+  subscription: Subscription | undefined;
+  segundosRestantes: any;
 
   ngOnInit(): void {
+    this.subscription = interval(1000).subscribe(x => {this.getTimeDifference();});
+  }
+
+  private getTimeDifference()
+  {
+    this.timeDifference = this.dataCasamento.getTime() - this.dataAtual.getTime();
+    this.allocateTimeUnits(this.timeDifference)
+  }
+
+  private allocateTimeUnits(timeDifference: any)
+  {
+    this.segundosRestantes = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.diasRestantes = Math.floor((timeDifference)/(this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
+
+
+  constructor() { }
+
+
+  ngOnDestroy(): void
+  {
+    this.subscription?.unsubscribe();
   }
 
 }
